2e3b15e8e3b033f0f9e3435ec53a2c99a89d243d
m0kita <papanegative@mail.ru>
2022-09-09 14:44:08 +0300

    Start project

---
+++ app/src/main/java/ru/m0kita/mokinote/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.m0kita.mokinote.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.m0kita.mokinote.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
